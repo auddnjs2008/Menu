8e2973141b12ff2dbbf2e29e38b7d649d288b192
auddnjs2008 <46766443+auddnjs2008@users.noreply.github.com>
2020-06-14 15:55:09 +0900

    menu finsih

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 const button = document.querySelector(".select");
 const bar = document.querySelector(".menu-bar");
+const numbers = document.querySelectorAll(".item");
+
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+
+const mains = document.querySelectorAll(".main");
 
 const handleClick = () => {
-  bar.classList.remove("hidden");
-  bar.classList.add("slider");
+  if (button.innerText === "Menu Start") {
+    bar.classList.add("slider");
+    bar.classList.remove("slideIn");
+    button.innerText = "Menu Close";
+  } else {
+    bar.classList.add("slideIn");
+    bar.classList.remove("slider");
+    button.innerText = "Menu Start";
+  }
+};
+
+const addHidden = (item) => {
+  mains.forEach(function (it) {
+    if (item !== it.id) {
+      it.classList.add("hidden");
+    }
+  });
+};
+
+const numberClick = (event) => {
+  const who = event.target.id;
+  if (who === "1") {
+    one.classList.remove("hidden");
+    addHidden("one");
+  } else if (who === "2") {
+    two.classList.remove("hidden");
+    addHidden("two");
+  } else if (who === "3") {
+    three.classList.remove("hidden");
+    addHidden("three");
+  } else if (who === "4") {
+    four.classList.remove("hidden");
+    addHidden("four");
+  } else if (who === "5") {
+    five.classList.remove("hidden");
+    addHidden("five");
+  } else {
+    bar.classList.add("slideIn");
+    bar.classList.remove("slider");
+    button.innerText = "Menu Start";
+  }
 };
 
 const init = () => {
   button.addEventListener("click", handleClick);
+  numbers.forEach(function (i) {
+    i.addEventListener("click", numberClick);
+  });
 };
 
 init();
